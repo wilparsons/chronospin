caeea5f9cc8cd0e4ac1b2855e2e86cbcff2708b3
Wil Parsons <entrocraft@gmail.com>
2024-02-19 13:21:09 -0800

    Adding nested RainShift entropy to each random CPU clock bit.

--- clockspin.c
+++ clockspin.c
@@ -2,17 +2,12 @@
 #include "rainshift.h"
 
 uint64_t clockspin(void) {
-  uint64_t entropy = 0;
+  uint64_t entropy = time((void *) 0);
   unsigned char i = 0;
 
   while (i != 64) {
     entropy <<= 1;
-    entropy += rainshift(entropy) & 1;
-    entropy |= rainshift(entropy) & 1;
-    entropy ^= rainshift(entropy) & 1;
-    entropy >>= 1;
-    entropy <<= 1;
-    entropy |= clock() & 1;
+    entropy |= (rainshift(rainshift(entropy + clock()) + clock()) + clock()) & 1;
     i++;
   }
 
